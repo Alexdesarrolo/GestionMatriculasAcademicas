c5a135774ddf4f539a5d9ad4ee4c8e6baeb81dc5
Alexdesarrolo <alexing.g@gmail.com>
2026-03-02 23:04:22 -0500

    se crea el servicio de crear usuario

--- src/main/java/com/ale/edu/gestionmatriculasacademicas/service/UserService.java
+++ src/main/java/com/ale/edu/gestionmatriculasacademicas/service/UserService.java
@@ -1,8 +1,11 @@
 package com.ale.edu.gestionmatriculasacademicas.service;
 
+import com.ale.edu.gestionmatriculasacademicas.domain.Authority;
 import com.ale.edu.gestionmatriculasacademicas.domain.User;
+import com.ale.edu.gestionmatriculasacademicas.repository.AuthorityRepository;
 import com.ale.edu.gestionmatriculasacademicas.repository.UserRepository;
 import com.ale.edu.gestionmatriculasacademicas.security.SecurityUtils;
+import com.ale.edu.gestionmatriculasacademicas.service.dto.AdminUserDTO;
 import com.ale.edu.gestionmatriculasacademicas.web.controller.errors.InvalidPasswordException;
 
 import org.slf4j.Logger;
@@ -10,6 +13,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,9 +28,13 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final AuthorityRepository authorityRepository;
+
+    public UserService(UserRepository userRepository,
+                       AuthorityRepository authorityRepository,PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authorityRepository = authorityRepository;
     }
 
     // Cambiar contraseña del usuario autenticado
@@ -44,5 +56,40 @@ public class UserService {
     @Transactional(readOnly = true)
     public java.util.Optional<User> getUserWithAuthoritiesByLogin(String login) {
         return userRepository.findOneWithAuthoritiesByLogin(login);
+    }
+
+    public User createUser(AdminUserDTO userDTO) {
+        User user = new User();
+        user.setLogin(userDTO.getLogin().toLowerCase());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        if (userDTO.getEmail() != null) {
+            user.setEmail(userDTO.getEmail().toLowerCase());
+        }
+        user.setImageUrl(userDTO.getImageUrl());
+        if (userDTO.getLangKey() == null) {
+            //user.setLangKey(Constants.DEFAULT_LANGUAGE); // default language
+        } else {
+            user.setLangKey(userDTO.getLangKey());
+        }
+        String encryptedPassword = passwordEncoder.encode("");//RandomUtil.generatePassword()
+        user.setPassword(encryptedPassword);
+        user.setResetKey(""); // RandomUtil.generateResetKey()
+        user.setResetDate(Instant.now());
+        user.setActivated(true);
+        if (userDTO.getAuthorities() != null) {
+            Set<Authority> authorities = userDTO
+                    .getAuthorities()
+                    .stream()
+                    .map(authorityRepository::findById)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toSet());
+            user.setAuthorities(authorities);
+        }
+        userRepository.save(user);
+        //this.clearUserCaches(user);
+        LOG.debug("Created Information for User: {}", user);
+        return user;
     }
 }
